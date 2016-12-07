6463bce8a221292c49d01b85b742bc41218bd606
Geek-zy <zy251021@163.com>
2016-12-07 17:58:15 +0800

    冒泡算法

---
+++ Bubble_Algorithm/Bubble_Algorithm.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int a[10] = {1,4,7,2,5,8,3,6,9,0};
+	int i, j, t = 0;
+
+	for(i = 0; i <= 9; i++) {
+		
+		printf("%d\t", a[i]);
+	}
+	
+	for(i = 0; i < 9; i++)
+	  for(j = i+1; j < 10 ; j++)
+		if(a[j] < a[i]) {
+			
+			t = a[j];
+			a[j] = a[i];
+			a[i] = t;
+		}
+printf("\n");
+	for(i = 0; i < 10; i++)
+	  printf("%d\t", a[i]);
+
+printf("\n");
+		  
+	return EXIT_SUCCESS;
+}
