ecd615ce0b99371e83189eb215ec8fb60705fbc5
Geek-zy <zy251021@163.com>
2018-07-30 16:08:30 +0800

    冒泡排序

--- Bubble_Algorithm/Bubble_Algorithm.c
+++ Bubble_Algorithm/Bubble_Algorithm.c
@@ -7,24 +7,26 @@ int main(int argc, char *argv[])
 	int a[10] = {1,4,7,2,5,8,3,6,9,0};
 	int i, j, t = 0;
 
-	for(i = 0; i <= 9; i++) {
+	for(i = 0; i < 10; i++) {
 		
 		printf("%d\t", a[i]);
 	}
 	
-	for(i = 0; i < 9; i++)
-	  for(j = i+1; j < 10 ; j++)
-		if(a[j] < a[i]) {
+	for(i = 0; i < 10; i++)
+	  for(j = i+1; j < 10; j++)
+		if(a[i] > a[j]) {
 			
 			t = a[j];
 			a[j] = a[i];
 			a[i] = t;
 		}
-printf("\n");
+
+	printf("\n");
+
 	for(i = 0; i < 10; i++)
 	  printf("%d\t", a[i]);
 
-printf("\n");
+	printf("\n");
 		  
 	return EXIT_SUCCESS;
 }
